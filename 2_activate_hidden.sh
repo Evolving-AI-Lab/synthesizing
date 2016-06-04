60462ad3b6f47f3b6fbd3e884afd8ad0e73bcde6
Anh Nguyen <anh.ng8@gmail.com>
2016-06-04 10:53:23 -0600

    Updated

--- 2_activate_hidden.sh
+++ 2_activate_hidden.sh
@@ -1,17 +1,14 @@
 #/bin/bash
 
-if [ "$#" -ne "1" ]; then
-  echo "Provide 1 output channel number at a conv5, e.g. 151"
-  exit 1
-fi
-
-# Get label for each unit
-path_labels="synset_words.txt"
-IFS=$'\n' read -d '' -r -a labels < ${path_labels}
+# Take in an unit number
+# if [ "$#" -ne "1" ]; then
+#   echo "Provide 1 output channel number at a conv5, e.g. 151"
+#   exit 1
+# fi
 
 opt_layer=fc6
 act_layer=conv5
-units="${1}"
+units="35 68 100" #"${1}"
 xy=6  # spatial location (6, 6)
 
 # Hyperparam settings for visualizing AlexNet
@@ -24,7 +21,7 @@ end_lr=1e-10
 clip=0
 multiplier=3
 bound_file=act_range/${multiplier}x/${opt_layer}.txt
-init_file="None" #"images/cat.jpg"
+init_file="None"
 
 # Debug
 debug=0
@@ -38,7 +35,7 @@ output_dir="output"
 rm -rf ${output_dir}
 mkdir ${output_dir}
 
-# Running optimization across a sweep of hyperparams
+# Sweeping across hyperparams
 for unit in ${units}; do
 
   for seed in {0..0}; do
@@ -66,9 +63,20 @@ for unit in ${units}; do
               --debug ${debug} \
               --output_dir ${output_dir} \
               --init_file ${init_file}
+          
+          f=${output_dir}/${act_layer}_${unit_pad}_${n_iters}_${L2}_${lr}__${seed}.jpg
+          list_files="${list_files} ${f}"
+
         done
       done
     done
   
   done
 done
+
+# Make a collage
+output_file=${output_dir}/example1.jpg
+montage ${list_files} -tile 5x1 -geometry +1+1 ${output_file}
+convert ${output_file} -trim ${output_file}
+echo "=============================="
+echo "Result of example 2: [ ${output_file} ]"
