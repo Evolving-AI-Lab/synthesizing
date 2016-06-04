c744aa9437bb4aa0572674e7dc0b0c66c5e9f275
Anh Nguyen <anh.ng8@gmail.com>
2016-06-03 21:45:29 -0600

    Updated

--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
 caffe_root = "/home/anh/src/caffe_latest/python" 
 gpu = True
 
-# Encoder
-encoder_path = "nets/caffenet/caffenet.caffemodel"
-encoder_definition = "nets/caffenet/caffenet.prototxt"
+# DNN being visualized
+net_weights = "nets/caffenet/caffenet.caffemodel"
+net_definition = "nets/caffenet/caffenet.prototxt"
 
-decoder_path = "nets/upconv/fc6/generator.caffemodel"
-decoder_definition = "nets/upconv/fc6/generator.prototxt"
+# Generator DNN
+generator_weights = "nets/upconv/fc6/generator.caffemodel"
+generator_definition = "nets/upconv/fc6/generator.prototxt"
+
+# Encoder DNN
+encoder_weights = "nets/caffenet/caffenet.caffemodel"
+encoder_definition = "nets/caffenet/caffenet.prototxt"
