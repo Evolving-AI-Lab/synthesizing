0326327cf327c0b5964dcad8578a981aa798c1dc
Anh Nguyen <anh.ng8@gmail.com>
2016-06-04 00:35:26 -0600

    Updated

--- 4_activate_output_placesCNN.sh
+++ 4_activate_output_placesCNN.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 if [ "$#" -ne "1" ]; then
-  echo "Provide 1 output unit number e.g. 945 for bell pepper."
+  echo "Provide 1 output unit number e.g. 198 for waiting room (MIT Places 205)."
   exit 1
 fi
 
