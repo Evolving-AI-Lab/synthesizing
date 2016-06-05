d392fee9b95f1f26754c11f53574c6a1339b27dd
Anh Nguyen <anh.ng8@gmail.com>
2016-06-04 21:06:04 -0600

    Updated

--- nets/caffenet/download.sh
+++ nets/caffenet/download.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+f=bvlc_reference_caffenet.caffemodel
+
+if [ ! -f "${f}" ]; then 
+  echo "Downloading..."
+  wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+fi
+
+echo "Done."
