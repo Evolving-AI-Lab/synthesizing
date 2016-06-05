3afbf4091c5fde02a775a1d0af35a50bc9d587ad
Anh Nguyen <anh.ng8@gmail.com>
2016-06-04 21:04:41 -0600

    Updated

--- nets/caffenet/download_caffenet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
--- nets/placesCNN/download.sh
+++ nets/placesCNN/download.sh
@@ -2,7 +2,15 @@
 f=placesCNN.tar.gz
 
 if [ ! -f ${f} ]; then
+  echo "Downloading..."
   wget http://places.csail.mit.edu/model/${f}
 fi
 
-tar xvf placesCNN.tar.gz
+echo "Extracting ${f}..."
+tmp="tmp"
+mkdir -p ${tmp}
+tar xvf placesCNN.tar.gz -C ${tmp}/
+mv ${tmp}/places205CNN_iter_300000.caffemodel ./
+rm -rf ${tmp}
+
+echo "Done."
